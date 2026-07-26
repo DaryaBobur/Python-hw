376e4068b8659fa6ea39b2b5f42a22168486ed19
Daria <97470059+DaryaBobur@users.noreply.github.com>
2026-07-26 17:50:46 +0300

    Answer 8.3 hw

---
+++ lesson_8/lesson8.3_hw.py
@@ -0,0 +1,19 @@
+# Вам необхідно написати функцію find_unique_value, яка приймає список із чисел,
+# знаходить серед них унікальне число та повертати його.
+# Унікальне число - це число, яке зустрічається в списку один раз.
+# Випадок, коли в одному списку буде кілька унікальних чисел, перевіряти не потрібно.
+
+# Answer
+
+def find_unique_value(some_list):
+
+   for num in some_list:
+       if some_list.count(num) == 1:
+           print(num)
+           return num
+   return None
+
+assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+print("ОК")
