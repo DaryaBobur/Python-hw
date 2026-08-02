ad6acc2fa4e14adb92597146c6ab0f89f21e6814
Daria <97470059+DaryaBobur@users.noreply.github.com>
2026-08-02 15:35:56 +0300

    Answer 10.3 hw

---
+++ lesson_10/lesson10.3_hw.py
@@ -0,0 +1,17 @@
+# Ваша функція is_even повинна повертати True якщо число парне, і False якщо число непарне.
+# Вхідні дані: Ціле число.
+# Вихідні дані: Логічний тип.
+
+# Answer
+
+def is_even(digit):
+    """ Перевірка чи є парним число """
+    if digit % 2 == 0:
+        return True
+    else:
+        return False
+
+assert is_even(2) == True, 'Test1'
+assert is_even(5) == False, 'Test2'
+assert is_even(0) == True, 'Test3'
+print('OK')
