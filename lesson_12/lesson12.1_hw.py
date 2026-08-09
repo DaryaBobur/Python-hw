ff5408b8766df7da360788314a733b4fce2e0405
Daria <97470059+DaryaBobur@users.noreply.github.com>
2026-08-09 18:19:16 +0300

    Answer 12.1 hw

---
+++ lesson_12/lesson12.1_hw.py
@@ -0,0 +1,26 @@
+# Напишіть функцію-генератор prime_generator, яка повертатиме прості числа.
+# Верхня межа цього діапазону визначається параметром цієї функції.
+# Наприклад, виклик функції
+# list(prime_generator(10)) повинен відповідати послідовності з чисел [2, 3, 5, 7].
+# Наступне число в цій послідовності - 11 і воно більше 10 тому воно не потрапляє в цей ряд
+
+# Answer
+
+def prime_generator(end):
+    numbers = list(range(2,end + 1))
+
+    for i in numbers:
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            yield i
+
+from inspect import isgenerator
+
+gen = prime_generator(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+print('Ok')
