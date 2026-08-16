1e6703a53a3d92a16aad506f89a629f6f413b584
Daria <97470059+DaryaBobur@users.noreply.github.com>
2026-08-16 23:02:56 +0300

    update

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello World")
+# print("Hello World")
+
+def closure_example(x):
+    """
+    Реалізує функцію, яка використовує замикання для збереження значення.
+
+    :param x: Початкове значення.
+    :return: Функція, яка використовує замикання для збереження значення x.
+    """
+    c = x
+    def inner_function(y):
+        print(x+y)
+        return x + y
+    return inner_function
+
+
+closure_instance = closure_example(-8)
+closure_instance(10)
+# Перевірка
+# assert closure_instance(3) == 8
+
+
+def outer_function(x):
+    def inner_function(y):
+        return x + y
+    return inner_function
+
+closure = outer_function(10)
+result = closure(7)
+print(result)
