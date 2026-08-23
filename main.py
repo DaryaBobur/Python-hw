6e8b6c131a30000bdc3fca7f43d4a66a7673d7a5
Daria <97470059+DaryaBobur@users.noreply.github.com>
2026-08-23 21:34:32 +0300

    Answer 16.2 hw

--- main.py
+++ main.py
@@ -1,30 +1,4 @@
 # print("Hello World")
 
-def closure_example(x):
-    """
-    Реалізує функцію, яка використовує замикання для збереження значення.
-
-    :param x: Початкове значення.
-    :return: Функція, яка використовує замикання для збереження значення x.
-    """
-    c = x
-    def inner_function(y):
-        print(x+y)
-        return x + y
-    return inner_function
-
-
-closure_instance = closure_example(-8)
-closure_instance(10)
-# Перевірка
-# assert closure_instance(3) == 8
-
-
-def outer_function(x):
-    def inner_function(y):
-        return x + y
-    return inner_function
-
-closure = outer_function(10)
-result = closure(7)
-print(result)
+a = [5, 6, 7, 8, 9]
+print(a[slice(None)])
